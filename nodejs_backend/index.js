275ae00e6130a59ce727758d1ad29cc69cd26bf9
TheHKT <hworschinski@gmail.com>
2022-08-12 15:44:51 +0200

    BACKEND: fixed Problem with DB

--- nodejs_backend/index.js
+++ nodejs_backend/index.js
@@ -2,49 +2,44 @@ const config = require('./config/config.json');
 const DB_config = require("./config/dbConfig.json");
 const express = require("express");
 const axios = require("axios"); //TODO might not need axios
-const dbConnection = require("tedious").Connection;
-const dbRequest = require("tedious").Request;
+const sql = require("mssql");
 
 async function main() {
     const app = express();
     app.use(express.json());
-    const connection = new dbConnection(DB_config);
-
-    // Attempt to connect and execute queries if connection goes through
-    connection.on("connect", err => {
-        err ? console.error(err.message) : console.log("connected to DB");     
-    });
-
-    connection.connect();
-
-    function queryDatabase() {
-        console.log("Reading rows from the Table...");
-
-        // Read all rows from table
-        const request = new dbRequest(
-            `SELECT * FROM Player`,
-            (err, rowCount) => {
-                if (err) {
-                    console.error(err.message);
-                } else {
-                    console.log(`${rowCount} row(s) returned`);
-                }
-            }
-        );
-
-        request.on("row", columns => {
-            columns.forEach(column => {
-                console.log("%s\t%s", column.metadata.colName, column.value);
-            });
-        });
-
-        connection.execSql(request);
+    try {   //connecting to DB
+        await sql.connect(DB_config)
+        console.log("connected to db...")
+    } catch (err) {
+        console.err("could not connect to db...");
     }
 
     app.get("/status", async (req, res) => {
         try {
             res.status(200).send('{"status":"Backend is running!"}');
         } catch (err) {
+            res.status(500).send(err);
+        }
+    });
+
+    app.put("/login", async (req, res) => {
+        try {
+            const inputData = req.body;
+            const result = await sql.query`select * from Player`
+            console.dir(result)
+            res.status(200).send(result)
+        } catch (err) {
+            res.status(500).send(err);
+        }
+    })
+
+    app.post("/registerPlayer", async (req, res) => {
+        try {
+            const data = req.body;
+            const request = await sql.query(`INSERT INTO Player(username, email, pw) VALUES ('${data.username}', '${data.email}', '${data.pw}');`)
+            res.status(200).send(request)
+        } catch (err) {
+            console.log(data);
             res.status(500).send(err);
         }
     });
