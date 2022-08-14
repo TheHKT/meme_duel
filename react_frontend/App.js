b4603383dbf9cd8ee12cdecb11137f3d80b733e6
TheHKT <hworschinski@gmail.com>
2022-08-14 21:46:14 +0200

    added some test stuff etc. I am hyped af

--- react_frontend/App.js
+++ react_frontend/App.js
@@ -1,18 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import { useState } from 'react';
 
 export default function App() {
-  const [name, setName] = useState("freegzuz");
+  const [name, setName] = useState("Hektor");
+  const [person, setPerson] = useState({name:"mario", age: 40});
 
   const clickHandler = () => {
-    setName("Suck my Duck!")
+    setName("Paul");
+    setPerson({name: "Luigi", age: 69})
   }
 
   return (
     <View style={styles.container}>
-      <View style={styles.header}>  
+      <View>  
       <Text style={styles.boldText}>My name is {name}</Text>
+      <Text style={styles.boldText}>His name is {person.name} and his age is {person.age}</Text>
+      <Text>Enter your name:</Text>
+      <TextInput style={styles.input} placeholder="mein Name" onChangeText={(value) => setName(value)}/>
       </View>
       <View style={styles.button}>
         <Button title="Update state" onPress={clickHandler}/>
@@ -43,6 +48,13 @@ const styles = StyleSheet.create({
   },
   button :{
     marginTop: 20
+  },
+  input : {
+    borderWidth: 1,
+    borderColor: "#777",
+    padding: 8,
+    margin: 10,
+    width: 200
   }
 
 });
